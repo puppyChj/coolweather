5477870c5fff137ea7141d5259fa9bc3101053c6
chj <277466060@qq.com>
2017-04-26 22:13:54 +0800

    完成遍历省市县三级列表的功能。

---
+++ app/src/main/java/com/coolweather/android/activity/MainActivity.java
@@ -0,0 +1,19 @@
+package com.coolweather.android.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.coolweather.android.R;
+
+/**
+ * Created by asuspc on 2017/4/26.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        setContentView(R.layout.activity_main);
+    }
+}
