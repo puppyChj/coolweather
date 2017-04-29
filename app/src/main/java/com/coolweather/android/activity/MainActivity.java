5e3d0b3320cbd9bad6c05b6d497b131a42e4b369
chj <277466060@qq.com>
2017-04-29 14:33:56 +0800

    加入显示天气信息的功能。

--- app/src/main/java/com/coolweather/android/activity/MainActivity.java
+++ app/src/main/java/com/coolweather/android/activity/MainActivity.java
@@ -1,6 +1,9 @@
 package com.coolweather.android.activity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 
 import com.coolweather.android.R;
@@ -15,5 +18,11 @@ public class MainActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstance){
         super.onCreate(savedInstance);
         setContentView(R.layout.activity_main);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if (prefs.getString("weather",null) != null){
+            Intent intent = new Intent(this,WeatherActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,14 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by asuspc on 2017/4/27.
+ */
+
+public class AQI {
+
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
